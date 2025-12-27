e582770517c2313a160d8b46054a6f286633e74f
Rohan Rai <92577040+Rohanrai540@users.noreply.github.com>
2025-12-27 09:27:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,13 @@
-// Select all price elements
-const prices = document.querySelectorAll(".prices");
+const button = document.querySelector("button");
+const ans = document.getElementById("ans");
 
-let total = 0;
+button.addEventListener("click", () => {
+  const prices = document.querySelectorAll(".prices");
+  let total = 0;
 
-// Calculate total price dynamically
-prices.forEach(price => {
-  total += Number(price.innerText);
+  prices.forEach(price => {
+    total += Number(price.innerText);
+  });
+
+  ans.innerText = total;
 });
-
-// Create new row
-const totalRow = document.createElement("tr");
-
-// Create single cell
-const totalCell = document.createElement("td");
-totalCell.setAttribute("colspan", "2");
-totalCell.innerText = `Total Price: ${total}`;
-
-// Append cell to row
-totalRow.appendChild(totalCell);
-
-// Append row to table
-document.querySelector("#grocery-table").appendChild(totalRow);
-
-
